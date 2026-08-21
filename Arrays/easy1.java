1bbb06e58cd9db1fa1f3ed3970ed957ae3114041
Siddhartha D <143932711+7-Sid-7@users.noreply.github.com>
2026-08-21 17:31:18 +0530

    Create easy1.java

---
+++ Arrays/easy1.java
@@ -0,0 +1,21 @@
+import java.util.Scanner;
+public class Main{
+  public static void main(String[] args){
+    Scanner in=new Scanner(System.in);
+    int n=in.nextInt();
+    int[] arr=new int[n];
+    for(int i=0;i<n;i++){
+      arr[i]=in.nextInt();
+    }
+    System.out.println(largestElement(arr));
+  }
+  static int largestElement(int[] nums){
+        int max=nums[0];
+        for(int i=1;i<nums.length;i++){
+            if(nums[i]>max){
+                max=nums[i];
+            }
+        }
+        return max;
+    }
+}
