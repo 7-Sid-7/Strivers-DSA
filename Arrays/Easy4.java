0d8755b06d1e044425c838048e4f578278a84b07
Siddhartha D <143932711+7-Sid-7@users.noreply.github.com>
2026-08-21 17:45:34 +0530

    Create Easy4.java

---
+++ Arrays/Easy4.java
@@ -0,0 +1,58 @@
+import java.util.*;
+
+public class Easy4{
+    public static void main(String[] args){
+       Scanner in=new Scanner(System.in);
+       int n=in.nextInt();
+       int[] nums=new int[n];
+       for(int i=0;i<n;i++){
+           nums[i]=in.nextInt();
+       }
+       System.out.println(removedup(arr));
+      System.out.println(removedup2(arr));
+    }
+  static int removedup(int[] arr){
+        int key=0;
+        int count=1;
+        int swap=1;
+        int j=0;
+        for(int i=0;i<arr.length-1;i++){
+            if(arr[i]==arr[i+1]){
+                key=arr[i+1];
+            }
+            j=i+1;
+            while(arr[j]==key){
+                j++;
+            }
+            if(arr[j]!=key){
+                key=arr[j];
+                count++;
+                int temp=arr[swap];
+                arr[swap]=arr[j];
+                arr[j]=temp;
+            }
+            swap++;
+            i=j;
+        }
+        System.out.println(Arrays.toString(arr));
+        return count;
+    }
+    static int remdup2(int[] arr){
+        int key=arr[0];
+        int swaps=0;
+        for(int i=1;i<arr.length-1;i++){
+            while(arr[i]<=key){
+                break;
+            }
+             if(arr[i]>key){
+                swaps++;
+                int temp=arr[swaps];
+                arr[swaps]=arr[i];
+                arr[i]=temp;
+                key=arr[swaps];
+            }
+        }
+        System.out.println(Arrays.toString(arr));
+        return swaps+1;
+    }
+}
